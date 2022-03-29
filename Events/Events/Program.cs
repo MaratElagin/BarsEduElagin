b8c800b6415268e0c514a4afd672666c5cc10928
Marat Elagin <maratelagin@gmail.com>
2022-03-29 18:15:34 +0300

    Updated readme. Events task complete

---
+++ Events/Events/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //Создание экземпляра обработчика
+        var handler = new KeyPressHandler();
+        
+        //Создание обработчика события. При совершении события - выводим на экран введённый символ
+        handler.OnKeyPressed += symbol => Console.WriteLine(symbol);
+        
+        //Запускаем обработчик
+        handler.Run();
+    }
+}
+class KeyPressHandler
+{
+    //Создание делегата для события
+    public delegate void EventHandler(char symbol);
+
+    //Определение события
+    public event EventHandler? OnKeyPressed; 
+    
+    public void Run()
+    {
+        char symbol;
+        do
+        {
+            symbol = (char) Console.Read();
+            OnKeyPressed?.Invoke(symbol); //Вызов события
+        } while (symbol != 'c');
+    }
+}
